51558c1938db953ae81556586f36d1c55ff8b66e
George Sproston <gsproston94@gmail.com>
2019-02-24 09:48:19 +0000

    all tiles now use a grass texture

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -7,11 +7,11 @@ Tile::Tile(const int x, const int y):
 
 void Tile::draw(sf::RenderWindow& window, const int iOffsetX, const int iOffsetY) const
 {
-	// rectangle shape
-	sf::RectangleShape shape(sf::Vector2f(TILE_SIZE, TILE_SIZE));
-	shape.setFillColor(sf::Color::Black);
-	shape.setOutlineColor(sf::Color::Green);
-	shape.setOutlineThickness(1);
-	shape.setPosition(sf::Vector2f((float)m_ix*TILE_SIZE + iOffsetX, (float)m_iy*TILE_SIZE + iOffsetY));
-	window.draw(shape);
+	sf::Texture texture;
+	texture.loadFromFile("images/tiles/grass.png");
+	sf::Sprite sprite;
+	sprite.setTexture(texture, true);
+	sprite.setPosition(sf::Vector2f((float)m_ix*TILE_SIZE + iOffsetX, (float)m_iy*TILE_SIZE + iOffsetY));
+	//Then, in PlayState::render()
+	window.draw(sprite);
 }
